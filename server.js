915c92ee8f0d08c69b53cfc211ab133ada370a65
Krunal Ashishbhai Kishnadwala <78363035+Mrkk108@users.noreply.github.com>
2022-03-09 15:13:01 +0530

    commit

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use("/api/users/", userRoute);
 app.use("/api/orders/", ordersRoute);
 
 
-if(process.env.NODE_ENV == 'production'){
-  app.use('/', express.static('client/build'))
+if(process.env.NODE_ENV === 'production'){
+  app.use('/', express.static('/client/build'))
   app.get('*',(req , res)=>{
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
   })
 }
 
