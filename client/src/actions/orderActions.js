636df1f3d7f3c96a4a9c02fbbb6eadb95ccf3f2c
Krunal Ashishbhai Kishnadwala <87028427+Mrkk1008@users.noreply.github.com>
2022-03-17 11:43:10 +0530

    Update orderActions.js

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -34,4 +34,33 @@ export const getUserOrders = () => async (dispatch , getState) => {
     dispatch({ type: "GET_USER_ORDERS_FAILED", payload: error });
   }
 }; 
+
+
+export const getAllOrders = () => async (dispatch, getState) => {
+
+  const currentUser = getState().loginUserReducer.currentUser;
+  dispatch({ type: "GET_ALL_ORDERS_REQUEST" });
+
+  try {
+    const response = await axios.get("/api/orders/getallorders");
+    console.log(response);
+    dispatch({ type: "GET_ALL_ORDERS_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "GET_ALL_ORDERS_FAILED", payload: error });
+  }
+};
+
+export const deliverOrder = (orderid) => async dispatch => {
+  
+  try {
+    const response = await axios.post("/api/orders/deliverorder" , {orderid})
+    console.log(response);
+    alert('Order Delivered');
+
+    const orders = await axios.get("/api/orders/getallorders");
+    dispatch({ type: "GET_ALL_ORDERS_SUCCESS", payload: orders.data})
+  } catch (error) {
+    console.log(error);
+  }
+}
    
