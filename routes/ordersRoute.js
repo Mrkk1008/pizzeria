8227a4a322f65cb8063d5b0dabace0c0187e1d7f
Krunal Ashishbhai Kishnadwala <87028427+Mrkk1008@users.noreply.github.com>
2022-03-17 11:40:26 +0530

    Deliver route added

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -57,4 +57,25 @@ router.post("/getuserorders" , async (req , res) =>{
   }
 });
 
+router.get("/getallOrders" ,async (req , res) =>{
+  try {
+    const orders = await Order.find({});
+    res.send(orders);
+  } catch (error) {
+    return res.status(400).json({ message: "something went wrong" + error });
+  }
+});
+
+router.post("/deliverorder" , async(req , res)=>{
+   const orderid = req.body.orderid;
+   try {
+     const order = await Order.findOne({_id : orderid})
+     order.isDelivered = true;
+     await order.save();
+     res.send('Order Delivered Successfully')
+   } catch (error) {
+     return res.status(400).json({ message: "something went wrong" + error });
+   }
+});
+
 module.exports = router;
